b00893d71973d9a9fdd9b3e6ac11cc7078ade8e6
Alejandro  Martínez <tortugal2004@gmail.com>
2024-03-09 14:33:25 -0500

    añadido el footer a medidas

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './header/Header'
 import Body from './body/Body'
 import Footer from './footer/Footer'
+import Carousel from './carousel/Carousel'
 
 function Index() {
   return (
     <div>
         <Header/>
+
         <Body/>
         <Footer/>
 
