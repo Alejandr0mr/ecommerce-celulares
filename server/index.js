23933a76344e3710c3d19f75f01a894dd8f3acf9
Alejandro <tortugal2004@gmail.com>
2024-06-05 13:57:51 -0500

    agregado contenido al index.js

--- server/index.js
+++ server/index.js
@@ -9,9 +9,26 @@ app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Saludando por el back");
+
+    let config = {
+        method: "GET",
+        maxBodyLength: Infinity,
+        url: 'https://api.jsonbin.io/v3/b/664e413dacd3cb34a84c01ea',
+        headers: {
+          'Content-Type': 'application/json',
+          "X-Master-Key": "$2a$10$G4Gi.QvGosjzduabWZjmXugLWUU0yL9vTiVSExFzwfLbyHy66RRr6"
+        }
+      };
+
+    axios(config)
+    .then(result=>{
+        res.send(result.data.record)
+    })
 });
 
+
 const userController = require("./controller/userController");
+const { default: axios } = require('axios');
 // app.post('/registro-usuario', userController.register);
 // app.use('/login', userController.login)
 app.post('/orden', userController.orden)  //Enviamos por el app.post los datos de la orden al controlador.
